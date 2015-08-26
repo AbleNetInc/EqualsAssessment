a6d19296aa99ee9ee36f8ddad7366b22702c4697
HalosGhost <halosghost@archlinux.info>
2015-08-26 14:03:51 -0500

    Add first run of the SQL interface

--- README.rst
+++ README.rst
@@ -7,3 +7,6 @@ In an attempt to keep the tool small, fast and easily maintainable, the whole sy
 At the moment, we have the following dependencies:
 
 - `GHC (or the Haskell Platform) >= 7.10 <https://www.haskell.org/downloads>`_
+- `sqlite3 >= 0.5.2.2 <https://hackage.haskell.org/package/sqlite-0.5.2.2>`_
+
+Note that the ``sqlite3`` Haskell package includes a few other dependencies and will require that ``sqlite3`` itself be installed.
---
+++ src/EqSQL.hs
@@ -0,0 +1,28 @@
+module EqSQL where
+
+import EqCommon
+import Database.SQLite
+
+saveForm :: EqVersion -> Form -> IO (Maybe String)
+saveForm v f = do let dbName = "EqDB"
+                  s <- openConnection dbName
+                  let query = "INSERT INTO " ++ show v ++ " VALUES (\"" ++ (fst f) ++ "\",\"" ++ toCSV f ++ "\")"
+                  e <- execStatement_ s query
+                  closeConnection s
+                  return e
+
+delForm :: EqVersion -> String -> IO (Maybe String)
+delForm v i = do let dbName = "EqDB"
+                 s <- openConnection dbName
+                 let query = "DELETE FROM " ++ show v ++ " WHERE id='" ++ i ++ "'"
+                 e <- execStatement_ s query
+                 closeConnection s
+                 return e
+
+retrieveForm :: EqVersion -> String -> IO (Either String [[Row String]])
+retrieveForm v i = do let dbName = "EqDB"
+                      s <- openReadonlyConnection dbName
+                      let query = "SELECT * FROM " ++ show v ++ " WHERE id='" ++ i ++ "'"
+                      e <- execStatement s query
+                      closeConnection s
+                      return e
