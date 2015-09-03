dcaf5fef396660bb55e6cb9f24181cdca7988f58
HalosGhost <halosghost@archlinux.info>
2015-09-03 14:24:34 -0500

    Add shim Main module

---
+++ src/Main.hs
@@ -0,0 +1,31 @@
+module Main where
+
+import EqCommon
+import EqLessons
+import EqSQL
+import System.Environment (getArgs)
+import System.Exit        (exitWith, ExitCode(ExitFailure, ExitSuccess))
+import Data.List
+
+version :: String
+version = "eqassess 2.1"
+
+usage :: String
+usage = intercalate "\n"
+      [ "Usage: eqassess [options]\n"
+      , "Options:"
+      , "  -h, --help      Print this help and exit"
+      , "  -v, --version   Print the version and exit"
+      ]
+
+main :: IO ()
+main = getArgs >>= parse >>= putStrLn
+
+exitFail, exitSucc :: IO String
+exitFail = exitWith $ ExitFailure 1
+exitSucc = exitWith   ExitSuccess
+
+parse :: [String] -> IO String
+parse a | elem "-h" a || elem "--help" a    = putStrLn usage   >> exitSucc
+        | elem "-v" a || elem "--version" a = putStrLn version >> exitSucc
+        | otherwise                         = putStrLn usage   >> exitFail
