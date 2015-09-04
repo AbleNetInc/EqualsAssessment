b2e6aa7fe5233f43d930e38854effc87c771b2d3
HalosGhost <halosghost@archlinux.info>
2015-09-04 09:33:48 -0500

    Radically simplify (and purify) main setup

--- src/Main.hs
+++ src/Main.hs
@@ -16,28 +16,41 @@ usage :: String
 usage = intercalate "\n"
       [ "Usage: eqassess [options]\n"
       , "Options:"
-      , "  -h, --help      Print this help and exit"
-      , "  -v, --version   Print the version and exit"
-      , "  -w, --web[=P]   Start the web interface on port P (8080 by default)"
+      , "  -h, --help         Print this help and exit"
+      , "  -v, --version      Print the version and exit"
+      , "  -d, --dbinit [N]   Initialize a db named N for eqassess (“EqDB” by default)"
+      , "  -w, --web [P]      Start the web interface on port P (8080 by default)"
       ]
 
 main :: IO ()
-main = getArgs >>= parse
+main = getArgs >>= dispatch . parseArgs
 
 exitFail, exitSucc :: IO ()
 exitFail = exitWith $ ExitFailure 1
 exitSucc = exitWith   ExitSuccess
 
-parse :: [String] -> IO ()
-parse a | a `hasArg` ("-h","--help"   ) = putStrLn usage   >> exitSucc
-        | a `hasArg` ("-v","--version") = putStrLn version >> exitSucc
-        | a `hasArg` ("-w","--web"    ) = runWebServer portNum
-        | otherwise                     = putStrLn usage   >> exitFail
-        where hasArg as (s,l)   = s `elem` as || l `elem` as
-              nextIdx o as      = (fromJust $ elemIndex o as) + 1
-              argOpt (s,l,d) as | elem s as && isValidIdx (n s) as = (read $ as !! n s) :: Int
-                                | elem l as && isValidIdx (n l) as = (read $ as !! n l) :: Int
-                                | otherwise                        = d
-                                where n o = nextIdx o as
-              isValidIdx n as   = n >= 0 && n < length as
-              portNum           = argOpt ("-w","--web",8080) a
+data EqAssessConf = Config { eqHelp    :: Bool
+                           , eqVersion :: Bool
+                           , eqInitDB  :: Bool
+                           , eqDbName  :: String
+                           , eqPortNum :: Int
+                           }
+
+dispatch :: EqAssessConf -> IO ()
+dispatch (Config h v i d p) | h         = putStrLn usage   >> exitSucc
+                            | v         = putStrLn version >> exitSucc
+                            | i         = mapM_ (initDB d) [Eq2]
+                            | p > 0     = runWebServer p
+                            | otherwise = putStrLn usage   >> exitFail
+
+parseArgs :: [String] -> EqAssessConf
+parseArgs as = Config { eqHelp    = as `hasArg` ("-h", "--help"   )
+                      , eqVersion = as `hasArg` ("-v", "--version")
+                      , eqInitDB  = as `hasArg` ("-d", "--dbinit" )
+                      , eqDbName  = optArg ("-d", "--dbinit","EqDB") as
+                      , eqPortNum = (read $ optArg ("-w", "--web", "8080") as) :: Int
+                      } where hasArg a (s,l)    = elem s a || elem l a
+                              nextArg s l       = (drop (1 + (fromJust $ elemIndex s l)) l) !! 0
+                              optArg (s,l,d) ls | elem s ls = nextArg s ls
+                                                | elem l ls = nextArg l ls
+                                                | otherwise = d
