6032f69de3d52f4747bdabe0eb07e5fb49bf40d0
HalosGhost <halosghost@archlinux.info>
2015-09-04 10:02:15 -0500

    Remove "default" options for simplicity

--- src/Main.hs
+++ src/Main.hs
@@ -6,8 +6,8 @@ import EqSQL
 import WebUI
 import System.Environment (getArgs)
 import System.Exit        (exitWith, ExitCode(ExitFailure, ExitSuccess))
-import Data.List
-import Data.Maybe
+import Data.List          (intercalate, elemIndex)
+import Data.Maybe         (fromJust)
 
 version :: String
 version = "eqassess 2.1"
@@ -16,10 +16,10 @@ usage :: String
 usage = intercalate "\n"
       [ "Usage: eqassess [options]\n"
       , "Options:"
-      , "  -h, --help         Print this help and exit"
-      , "  -v, --version      Print the version and exit"
-      , "  -d, --dbinit [N]   Initialize a db named N for eqassess (“EqDB” by default)"
-      , "  -w, --web [P]      Start the web interface on port P (8080 by default)"
+      , "  -h, --help       Print this help and exit"
+      , "  -v, --version    Print the version and exit"
+      , "  -d, --dbinit N   Initialize a db named N for eqassess"
+      , "  -w, --web P      Start the web interface on port P"
       ]
 
 main :: IO ()
@@ -33,24 +33,25 @@ data EqAssessConf = Config { eqHelp    :: Bool
                            , eqVersion :: Bool
                            , eqInitDB  :: Bool
                            , eqDbName  :: String
+                           , eqRunServ :: Bool
                            , eqPortNum :: Int
                            }
 
 dispatch :: EqAssessConf -> IO ()
-dispatch (Config h v i d p) | h         = putStrLn usage   >> exitSucc
-                            | v         = putStrLn version >> exitSucc
-                            | i         = mapM_ (initDB d) [Eq2]
-                            | p > 0     = runWebServer p
-                            | otherwise = putStrLn usage   >> exitFail
+dispatch (Config h v i d r p) | h         = putStrLn usage   >> exitSucc
+                              | v         = putStrLn version >> exitSucc
+                              | r         = runWebServer p
+                              | i         = mapM_ (initDB d) [Eq2]
+                              | otherwise = putStrLn usage   >> exitFail
 
 parseArgs :: [String] -> EqAssessConf
 parseArgs as = Config { eqHelp    = as `hasArg` ("-h", "--help"   )
                       , eqVersion = as `hasArg` ("-v", "--version")
                       , eqInitDB  = as `hasArg` ("-d", "--dbinit" )
-                      , eqDbName  = optArg ("-d", "--dbinit","EqDB") as
-                      , eqPortNum = (read $ optArg ("-w", "--web", "8080") as) :: Int
-                      } where hasArg a (s,l)    = elem s a || elem l a
-                              nextArg s l       = (drop (1 + (fromJust $ elemIndex s l)) l) !! 0
-                              optArg (s,l,d) ls | elem s ls = nextArg s ls
-                                                | elem l ls = nextArg l ls
-                                                | otherwise = d
+                      , eqRunServ = as `hasArg` ("-w", "--web"    )
+                      , eqDbName  = optArg ("-d", "--dbinit") as
+                      , eqPortNum = (read $ optArg ("-w", "--web") as) :: Int
+                      } where hasArg a (s,l)  = elem s a || elem l a
+                              nextArg s l     = l !! (1 + (fromJust $ elemIndex s l))
+                              optArg (s,l) ls | elem s ls = nextArg s ls
+                                              | elem l ls = nextArg l ls
