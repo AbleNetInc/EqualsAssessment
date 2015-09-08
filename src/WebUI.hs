3142058c6864dcf206b752bcc537c971f982d5e7
HalosGhost <halosghost@archlinux.info>
2015-09-08 08:59:13 -0500

    Initial forms

--- src/WebUI.hs
+++ src/WebUI.hs
@@ -15,13 +15,12 @@ import           Data.Foldable     (toList)
 import           Data.List         (intercalate)
 
 tbLesson :: Lesson -> Text
-tbLesson l = Text.pack $ "<td>" ++ intercalate "</td><td>" [c,s,o,n,a,r] ++ "</td>"
-       where c = show $ chapter l
-             s = [section l]
-             o = show $ count l
+tbLesson l = Text.pack $ "<td>" ++ intercalate "</td><td>" [n,s,a] ++ "</td>"
+       where s = "<input type=\"radio\" name=\"" ++ n ++ "_score\" value=\"-1\" checked> blank"
+              ++ "<input type=\"radio\" name=\"" ++ n ++ "_score\" value=\"0\"> 0"
+              ++ "<input type=\"radio\" name=\"" ++ n ++ "_score\" value=\"1\"> 1"
              n = Text.unpack $ lName l
-             a = if (score l) == (-1) then "" else show $ score l
-             r = show $ adaptedScore l
+             a = "<input type=\"checkbox\" name\"" ++ n ++ "_adapted\" value=\"adapted\">"
 
 runWebServer :: Int -> IO ()
 runWebServer pnum = Web.scotty pnum $ do Web.get "/:teacher/:student"
@@ -40,10 +39,9 @@ runWebServer pnum = Web.scotty pnum $ do Web.get "/:teacher/:student"
                                           , "<title>Equals Assessment</title>"
                                           , "<link rel='icon' href='",ico,"' type='image/x-icon' />"
                                           , "<link rel='shortcut icon' href='",ico,"' type='image/x-icon' />"
-                                          , "</head>"
-                                          , "<h1>Assessment by ",t," for ",s,":</h1>"
+                                          , "</head><body><h1>Assessment by ",t," for ",s,":</h1>"
+                                          , "<form method=\"post\" enctype=\"multipart/form-data\">"
                                           , "<table>"
                                           , mconcat trs
-                                          , "</table>"
-                                          , "</html>"
+                                          , "</table></form></body></html>"
                                           ]
