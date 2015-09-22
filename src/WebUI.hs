15cf795acbd96fa65f704b505b847e3f9edf30ba
HalosGhost <halosghost@archlinux.info>
2015-09-22 11:24:35 -0500

    Update available formats and mime-types

--- src/WebUI.hs
+++ src/WebUI.hs
@@ -193,8 +193,9 @@ runWebServer pnum = Web.scotty pnum $ do
                                                   input_ [type_ "submit", name_ "s", value_ "New"]; " "
                                                   input_ [type_ "submit", name_ "s", value_ "Export"];
                                                   select_ [name_ "ext", class_ "hidden"] $ do option_ [value_ "csv" ] "to CSV"
-                                                                                              option_ [value_ "tex" ] "to LaTeX"
+                                                                                              option_ [value_ "htm" ] "to HTML"
                                                                                               option_ [value_ "pdf" ] "to PDF"
+                                                                                              option_ [value_ "rtf" ] "to RTF"
                                                                                               option_ [value_ "docx"] "to Word"
                                                                                               option_ [value_ "xlsx"] "to Excel"
                                                   br_ []; br_ []
@@ -228,7 +229,9 @@ runWebServer pnum = Web.scotty pnum $ do
                                 "Export" -> do sf <- liftIO $ saveFile nas ext
                                                Web.setHeader "Content-Type" $ case ext of
                                                    "csv"  -> "text/csv"
+                                                   "htm"  -> "text/html; charset=UTF-8"
                                                    "pdf"  -> "application/pdf"
+                                                   "rtf"  -> "application/rtf"
                                                    "docx" -> "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
                                                    "xlsx" -> "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
                                                    _      -> "text/plain"
